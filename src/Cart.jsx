369b72376bec556d8eb07126a87186c3aee7cda0
sameer2407 <satyamjadaun2001@gmail.com>
2024-05-31 17:24:36 +0530

    Total Price Added

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -2,38 +2,58 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { clearCart } from "./utils/cartSlice";
-
 import ItemCartList from "./ItemCartList";
 import NoItems from "./NoItems";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
-
-  console.log(cartItems);
-
   const dispatch = useDispatch();
-  if (cartItems.length == 0) {
+
+  // Calculate the total price
+  const totalPrice = cartItems.reduce((total, item) => {
+    return total + item.card.info.price;
+  }, 0);
+
+  if (cartItems.length === 0) {
     return (
       <>
-        <NoItems></NoItems>
+        <NoItems />
       </>
     );
   }
 
   return (
-    <div className=" text-center m-4 p-4">
-      <h1 className="text-2xl font-bold">Cart</h1>
-      <div>
-        <button
-          className="p-2 m-2 bg-red-400 text-white rounded-lg"
-          onClick={() => {
-            dispatch(clearCart());
-          }}
-        >
-          Clear Cart
-        </button>
-        <ItemCartList items={cartItems}></ItemCartList>
+    <div className="text-center m-4 p-4">
+      <div className="flex justify-between">
+        <div>
+          <h1 className="text-xl font-bold p-6 text-gray-600 font-mono">
+            Your Selected Cart Items
+          </h1>
+        </div>
+        <div className="">
+          <h2 className="text-normal text-gray-600 font-bold">
+            Total Price: ₹ {totalPrice / 100}/-
+          </h2>
+          <button
+            className="p-2 mt-2 bg-green-500 text-white rounded-lg"
+            onClick={() => {
+              alert("Order placed!");
+            }}
+          >
+            Order
+          </button>
+          <button
+            className="p-2 m-2 bg-red-400 text-white rounded-lg"
+            onClick={() => {
+              dispatch(clearCart());
+            }}
+          >
+            Clear Cart
+          </button>
+        </div>
       </div>
+
+      <ItemCartList items={cartItems} />
     </div>
   );
 };
