13461a3de9ea48c9ef8407e9283f9665e10deceb
sameer2407 <satyamjadaun2001@gmail.com>
2024-05-31 12:26:16 +0530

    image path corrected

--- src/NoItems.jsx
+++ src/NoItems.jsx
@@ -4,7 +4,7 @@ const NoItems = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <img
-        src="src/utils/empty-cart.png"
+        src="/empty-cart.png"
         alt="Empty Cart"
         className="h-48 w-48 sm:h-64 sm:w-64 md:h-96 md:w-96 mb-4"
       />
